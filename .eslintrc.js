7c618202d55e1e17755df38b9815043d82f53d4b
Qiwei Yang <yangqiwei97@gmail.com>
2026-04-23 18:21:10 +0800

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@workspace/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
+  root: true,
+  ignorePatterns: [
+    "apps/**",
+    "packages/**",
+    "build/**",
+    "sherpa-onnx/**",
+    "whisper.cpp/**",
+  ],
 }
